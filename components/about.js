fe2b212e650b218dcfb3ca3735247b2bfdf59283
disapar <disaponline@yahoo.com.ar>
2021-05-02 01:39:42 -0300

    1 file modified

--- components/about.js
+++ components/about.js
@@ -1,4 +1,4 @@
 
 export const About =`<div class="animated fadeIn"><h1>¿Who I am?</h1>
-<p><img class="avatar" src="https://media-exp1.licdn.com/dms/image/C4E03AQG7x0jLq5SZ9Q/profile-displayphoto-shrink_100_100/0/1612651643754?e=1619049600&v=beta&t=gH5mvJHMKnskW08f0-YhDTWg8WYIaGypZYcw-pzqBXg"><br>My name is Adolfo G. Pérez, <br>
+<p><img class="avatar" src="https://scontent.fepa7-1.fna.fbcdn.net/v/t1.6435-9/88424889_10221383361269766_45632442677067776_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=_M4dIG_2GnsAX-_fQVQ&_nc_ht=scontent.fepa7-1.fna&oh=dd7ce1ffedbdde5bd7acf2f9dcc4ecf2&oe=60B56557"><br>My name is Adolfo G. Pérez, <br>
 I am Developer javascript </p></div>`;
